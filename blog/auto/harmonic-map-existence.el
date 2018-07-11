0bf09f602c58bdd29452e1c73f17c20a6ff8517d
darknmt <nguyenmanhtien88@gmail.com>
2018-07-11 23:07:42 +0200

    Content update

--- blog/auto/harmonic-map-existence.el
+++ blog/auto/harmonic-map-existence.el
@@ -3,12 +3,12 @@
  (lambda ()
    (LaTeX-add-labels
     "thm:bound-2-2"
-    "sec:org77ab882"
+    "sec:org50e3df5"
     "lem:bound-2-p"
     "eq:lem:bound-2-p:2"
     "eq:lem:bound-2-p:3"
     "thm:bound-k-p"
-    "sec:orgc8639fa"
+    "sec:orga0359a0"
     "thm:global-heat-existence"
     "eq:thm:global-heat"
     "thm:final"))
